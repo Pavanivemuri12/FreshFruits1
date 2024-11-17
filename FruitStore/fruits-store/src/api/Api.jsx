94168f038c7c8132d9099e079d39896fa725e8a0
Asma <asmabegum102004@gmail.com>
2024-11-18 00:03:25 +0530

    asma

--- FruitStore/fruits-store/src/api/Api.jsx
+++ FruitStore/fruits-store/src/api/Api.jsx
@@ -4,34 +4,16 @@ import axios from 'axios'
 const API = 'http://localhost:3000'
 
 // ProductEndPonts
-
-const addBasicFruits = (basicfruits) => axios.post(`${API}/basicfruits/add`,basicfruits)
-const addDryFruits = (dryfruits) => axios.post(`${API}/dryfruits/add`,dryfruits)
-const addExoticFruits = (exoticfruits) => axios.post(`${API}/exoticfruits/add`,exoticfruits)
-const addOrganicFruits = (organicfruits) => axios.post(`${API}/organicfruits/add`,organicfruits)
-const addSeasonalFruits = (seasonalfruits) => axios.post(`${API}/seasonalfruits/add`,seasonalfruits)
-const addFruitBaskets = (fruitbaskets) => axios.post(`${API}/fruitbaskets/add`,fruitbaskets)
-
-
-const deleteBasicFruits = (id) => axios.delete(`${API}/basicfruits/delete/${id}`)
-const deleteDryFruits = (id) => axios.delete(`${API}/dryfruits/delete/${id}`)
-const deleteExoticFruits = (id) => axios.delete(`${API}/exoticfruits/delete/${id}`)
-const deleteOrganicFruits = (id) => axios.delete(`${API}/organicfruits/delete/${id}`)
-const deleteSeasonalFruits = (id) => axios.delete(`${API}/Seasonalfruits/delete/${id}`)
-const deleteFruitBaskets = (id) => axios.delete(`${API}/fruitbaskets/delete/${id}`)
-
-const editBasicFruits = (basicfruits,id)=>axios.put(`${API}/basicfruits/edit/${id}`,basicfruits)
-const editDryFruits = (dryfruits,id)=>axios.put(`${API}/dryfruits/edit/${id}`,dryfruits)
-const editExoticFruits = (exoticfruits,id)=>axios.put(`${API}/exoticfruits/edit/${id}`,exoticfruits)
-const editOrganicFruits = (organicfruits,id)=>axios.put(`${API}/organicfruits/edit/${id}`,organicfruits)
-const editSeasonalFruits = (seasonalfruits,id)=>axios.put(`${API}/seasonalfruits/edit/${id}`,seasonalfruits)
-const editFruitBaskets = (exoticfruitbaskets,id)=>axios.put(`${API}/fruitbaskets/edit/${id}`,fruitbaskets)
-
-
 const getBasicFruits = () => axios.get(`${API}/basicfruits/all`)
+const addBasicFruits = (basicfruits) => axios.post(`${API}/basicfruits/add`,basicfruits)
+const deleteBasicFruits = (id) => axios.delete(`${API}/basicfruits/delete/${id}`)
+const editBasicFruits = (basicfruits,id)=>axios.put(`${API}/basicfruits/edit/${id}`,basicfruits)
+
 const getDryFruits = () => axios.get(`${API}/dryfruits/all`)
 const getExoticFruits = () => axios.get(`${API}/exoticfruits/all`)
+
 const getOrganicFruits = () => axios.get(`${API}/organicfruits/all`)
+const
 const getSeasonalFruits = () => axios.get(`${API}/seasonalfruits/all`)
 const getFruitBaskets = () => axios.get(`${API}/fruitbaskets/all`)
 const getNewlyAdded = () => axios.get(`${API}/newlyadded/all`)
@@ -44,29 +26,8 @@ export{
     getFruitBaskets,
 
     addBasicFruits,
-    addDryFruits,
-    addExoticFruits,
-    addOrganicFruits,
-    addSeasonalFruits,
-    addFruitBaskets,
-
-
-
     deleteBasicFruits,
-    deleteDryFruits,
-    deleteExoticFruits,
-    deleteOrganicFruits,
-    deleteSeasonalFruits,
-    deleteFruitBaskets,
-
-
-
-
     editBasicFruits,
-    editDryFruits,
-    editExoticFruits,
-
-
     getNewlyAdded
 }
 
